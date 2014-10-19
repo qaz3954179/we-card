5cace2d194ebc4054b959e8b0929c946b4d8f7c9
time <5042084@163.com>
2014-10-19 19:32:31 +0800

    weixin

--- web/app.js
+++ web/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var wechat = require('./routes/wechat');
 
 var app = express();
 
@@ -21,9 +22,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.query());
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/we-card', wechat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
